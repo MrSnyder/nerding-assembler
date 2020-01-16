8f921271ebec6854a0fb41950ac4d9b2055227c4
Alasdair Collinson <blalasaadri@googlemail.com>
2020-01-16 22:13:22 +0100

    Second commit

--- gcc/hello.c
+++ gcc/hello.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main () {
-    puts ("Hallo puts Welt!");
-    puts ("Hallo puts Welt newline!\n");
-    printf ("Hallo printf Welt!");
-    printf ("Hallo printf Welt newline!\n");
+    // puts ("Hallo puts Welt!");
+    // puts ("Hallo puts Welt newline!\n");
+    // printf ("Hallo printf Welt!");
+    // printf ("Hallo printf Welt newline!\n");
+    // printf ("Hallo printf with zero %d Welt!", 0);
+    // printf ("Hallo printf with zero %d Welt newline!\n", 0);
+    // printf ("Hallo printf with %d Welt!", 666);
+    // printf ("Hallo printf with %d Welt newline!\n", 666);
+    // printf ("Hallo printf with %d %d %d %d %d %d %d %d %d Welt!", 666, 777, 888, 999, 111, 222, 333, 444, 555);
+    // printf ("Hallo printf with %d %d %d %d %d %d %d %d %d Welt newline!\n", 666, 777, 888, 999, 111, 222, 333, 444, 555);
+    printf ("Hallo Welt! %d %d", 2, 5);
     return 0;
 }
--- gcc/hello.s
+++ gcc/hello.s
@@ -2,13 +2,7 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"Hallo puts Welt!"
-.LC1:
-	.string	"Hallo puts Welt newline!\n"
-.LC2:
-	.string	"Hallo printf Welt!"
-.LC3:
-	.string	"Hallo printf Welt newline!"
+	.string	"Hallo Welt! %d %d"
 	.text
 	.globl	main
 	.type	main, @function
@@ -20,15 +14,11 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	movl	$5, %edx
+	movl	$2, %esi
 	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
-	leaq	.LC1(%rip), %rdi
-	call	puts@PLT
-	leaq	.LC2(%rip), %rdi
 	movl	$0, %eax
 	call	printf@PLT
-	leaq	.LC3(%rip), %rdi
-	call	puts@PLT
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
@@ -36,5 +26,5 @@ main:
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.ident	"GCC: (Ubuntu 8.3.0-6ubuntu1) 8.3.0"
 	.section	.note.GNU-stack,"",@progbits
