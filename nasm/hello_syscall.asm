6b4df591fcba7860e2c8940f2cfbb89ce0bc5442
Markus Schneider <markus@beefcafe.de>
2020-01-17 23:09:03 +0100

    Added hello_syscall

---
+++ nasm/hello_syscall.asm
@@ -0,0 +1,18 @@
+; nasm -f elf64 hello_syscall.asm
+; ld -static -o hello_syscall -e start hello_syscall.o
+section .data
+        msg db "Hello World (via syscall)!", 0x0a
+        len equ $ - msg
+section .text
+global start:
+start:
+        mov eax, 4 ; 4=write
+        mov ebx, 1
+        mov ecx, msg
+        mov edx, len
+        int 0x80
+
+
+        mov eax, 1 ; 1=exit
+        mov ebx, 0        
+        int 0x80
